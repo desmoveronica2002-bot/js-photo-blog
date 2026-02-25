1fa900e07b4cb0eb36f5e455df79b6aad163e610
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-02-25 16:02:46 +0100

    start Milestone 3

--- script.js
+++ script.js
@@ -3,5 +3,35 @@
 
 
 // variabile per il link 
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
+
+const cardContainer = document.querySelector(".card-container"); //.card-container metto il punto perchè seleziono una classe e non un id
 
 
+fetch(endpoint)
+.then(res=>res.json())
+.then(data=>{
+    console.log(data);
+
+    data.forEach(element => {
+        console.log(element)
+
+        const {title,date,url} = element;
+
+        const markup = `
+        <div class="col-12 col-md-6 col-lg-4">
+            <img src="./img/pin.svg" alt="pallino" class="pallino">
+            <div class="card">
+                <img src="${url}" class="card-img-top" alt="${title}">
+                <div class="card-body">
+                    <p>${date}</p>
+                    <p>${title}</p>
+                </div>
+            </div>
+        </div>
+        `;
+        console.log(markup);
+        cardContainer.insertAdjacentHTML('beforeend', markup);
+        
+    });
+})
