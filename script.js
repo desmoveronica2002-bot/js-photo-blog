d1bc46d03fc7621255cb11b48305d68c49edddcd
Veronica Golinelli <desmoveronica2002@gmail.com>
2026-02-25 14:21:21 +0100

    add pallino

---
+++ script.js
@@ -0,0 +1,7 @@
+//Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API,
+//sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+
+
+// variabile per il link 
+
+
